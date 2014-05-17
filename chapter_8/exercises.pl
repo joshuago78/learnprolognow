713c335d9594546b0369ec83f5b074e4d2164b71
Joshua Gomez <gomezjn@lib-dev11.usc.edu>
2014-05-17 11:31:07 -0700

    Added solution to exercise 8.1

---
+++ chapter_8/exercises.pl
@@ -0,0 +1,45 @@
+% Exercise  8.1
+% Here.s our basic DCG:
+%
+%   s  -->  np,vp. 
+%    
+%   np  -->  det,n. 
+%    
+%   vp  -->  v,np. 
+%   vp  -->  v. 
+%    
+%   det  -->  [the]. 
+%   det  -->  [a]. 
+%    
+%   n  -->  [woman]. 
+%   n  -->  [man]. 
+%   n  -->  [apple]. 
+%   n  -->  [pear]. 
+%    
+%   v  -->  [eats].
+% Suppose we add the noun "men" (which is plural) and the verb "know". Then we
+% would want a DCG which says that "The men eat" is ok, "The man eats" is ok,
+% "The men eats" is not ok, and "The man eat" is not ok. Change the DCG so that
+% it correctly handles these sentences. Use an extra argument to cope with the
+% singular/plural distinction.
+
+
+s --> np(Agreement), vp(Agreement).
+
+np(Agreement) --> det(Agreement), n(Agreement).
+np(plural) --> n(plural).
+
+vp(Agreement) --> v(Agreement), np(_).
+
+det(_) --> [the].
+det(singular) --> [a].
+
+n(singular) --> [woman].
+n(singular) --> [man].
+n(singular) --> [apple].
+n(singular) --> [pear].
+n(plural) --> [men].
+
+v(singular) --> [eats].
+v(plural) --> [eat].
+
