7cd64bf827ded7e9104caf880603747a3335d12e
Joshua Gomez <gomezjn@lib-dev11.usc.edu>
2014-05-17 11:51:57 -0700

    Completed exercises for chapter 8

--- chapter_8/exercises.pl
+++ chapter_8/exercises.pl
@@ -43,3 +43,22 @@ n(plural) --> [men].
 v(singular) --> [eats].
 v(plural) --> [eat].
 
+
+
+
+% Exercise  8.2
+% In the text, we only gave examples of DCG rules with one extra argument, but
+% in fact you can add as many extra arguments as you like. Here.s a DCG rule
+% with three extra arguments:
+%
+%   kanga(V,R,Q)  -->  roo(V,R),jumps(Q,Q),{marsupial(V,R,Q)}.
+%
+% Translate it into the form Prolog uses.
+
+
+kanga(V, R, Q, A, C) :-
+  roo(V, R, A, B),
+  jumps(Q, Q, B, C),
+  marsupial(V,R,Q).
+
+
