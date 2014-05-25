5f128453e3af5c3e277abf65831e578fd5527019
Joshua Gomez <gomezjn@usc.edu>
2014-05-25 10:28:49 -0700

    Completed problem 1 of chapter 11 practical session

---
+++ chapter_11/practical.pl
@@ -0,0 +1,46 @@
+% 1. Sets can be thought of as lists that don.t contain any repeated elements.
+% For example, [a,4,6] is a set, but [a,4,6,a] is not (as it contains two
+% occurrences of a) . Write a Prolog program subset/2 that is satisfied when
+% the first argument is a subset of the second argument (that is, when every
+% element of the first argument is a member of the second argument). For
+% example:
+
+%   ?-  subset([a,b],[a,b,c]) 
+%   yes 
+    
+%   ?-  subset([c,b],[a,b,c]) 
+%   yes 
+    
+%   ?-  subset([],[a,b,c]) 
+%   yes
+%
+% Your program should be capable of generating all subsets of an input set by
+% backtracking. For example, if you give it as input
+
+%   ?-  subset(X,[a,b,c])
+
+% it should successively generate all eight subsets of [a,b,c] .
+
+
+%% Joshua's comments:
+%% This is a very tricky assignment. My answer below is position dependent.
+%% Thus, it gives both [a,b] and [b,a] as answers, which is still correct, but
+%% ends up with more than the required 8 solutions to subset(X, [a,b,c]).
+
+%% I looked up other solutions online and saw one in the form of  subset2/2.
+%% That solution correctly constructs the subsets backwards, but fails for the
+%% test of subset([b,c], [a,b,c]), which was given in the assignment.
+
+subset([], _).
+subset([H|T], Set) :-
+  cut(H, Set, Subset),
+  subset(T, Subset).
+
+cut(H, [H|T], T).
+cut(X, [H|T], [H|Subset]) :-
+  cut(X, T, Subset).
+  
+
+subset2([], []).
+subset2(Subset, [_|Set]) :- subset2(Subset, Set).
+subset2([H|Subset], [H|Set]) :- subset2(Subset, Set).
